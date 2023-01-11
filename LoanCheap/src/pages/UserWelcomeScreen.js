774ad451ae590449278797b09eeb04d8c0a7eec2
Ibrahem Chahine <22155702+ibrahimchahine@users.noreply.github.com>
2023-01-11 15:48:05 +0200

    Added comments to the admin controller

--- LoanCheap/src/pages/UserWelcomeScreen.js
+++ LoanCheap/src/pages/UserWelcomeScreen.js
@@ -1,62 +1,149 @@
-import { View, ScrollView, StyleSheet, Image } from 'react-native';
-import { Text, Tile } from '@rneui/themed';
-import React from 'react';
+import { View, ScrollView, StyleSheet, Image } from "react-native";
+import { Text, Tile } from "@rneui/themed";
+import React from "react";
 
 const UserWelcomeScreen = () => {
   return (
-    <View style={{flex: 1, position: 'relative'}}>
-    <Image
-      source={require('../images/LoanCheap.png')}
-      style={{width: 110, height: 110, position: 'relative', alignSelf: 'center'}}
-    />
-  <View style={{flex: 1, padding: 16}}>
-  <Text style={{fontSize: 24, fontWeight: 'bold', marginBottom: 16}}>Welcome to our app</Text>
-  <View style={{flexDirection: 'row', alignItems: 'center'}}>
-    <View style={{width: 24, height: 24, backgroundColor: '#05445E', borderRadius: 12, marginRight: 8}} />
-    <Text style={{fontSize: 18, fontWeight: 'bold'}}>As a customer you can:</Text>
-  </View>
-  <View>
-    <View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 8}}>
-      <Text style={{fontSize: 16, marginRight: 8}}>1.</Text>
-      <Text style={{fontSize: 16}}>Search for loans in the system</Text>
+    <View style={{ flex: 1, position: "relative" }}>
+      <Image
+        source={require("../images/LoanCheap.png")}
+        style={{
+          width: 110,
+          height: 110,
+          position: "relative",
+          alignSelf: "center",
+        }}
+      />
+      <View style={{ flex: 1, padding: 16 }}>
+        <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 16 }}>
+          Welcome to our app
+        </Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View
+            style={{
+              width: 24,
+              height: 24,
+              backgroundColor: "#05445E",
+              borderRadius: 12,
+              marginRight: 8,
+            }}
+          />
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+            As a customer you can:
+          </Text>
+        </View>
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 8,
+            }}
+          >
+            <Text style={{ fontSize: 16, marginRight: 8 }}>1.</Text>
+            <Text style={{ fontSize: 16 }}>Search for loans in the system</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 8,
+            }}
+          >
+            <Text style={{ fontSize: 16, marginRight: 8 }}>2.</Text>
+            <Text style={{ fontSize: 16 }}>Submit a request for loans</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 8,
+            }}
+          >
+            <Text style={{ fontSize: 16, marginRight: 8 }}>3.</Text>
+            <Text style={{ fontSize: 16 }}>
+              View pending and accepted loans you submitted
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{ flexDirection: "row", alignItems: "center", marginTop: 16 }}
+        >
+          <View
+            style={{
+              width: 24,
+              height: 24,
+              backgroundColor: "#05445E",
+              borderRadius: 12,
+              marginRight: 8,
+            }}
+          />
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+            As an admin you can:
+          </Text>
+        </View>
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 8,
+            }}
+          >
+            <Text style={{ fontSize: 16, marginRight: 8 }}>1.</Text>
+            <Text style={{ fontSize: 16 }}>
+              Create a new loan in the system
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 8,
+            }}
+          >
+            <Text style={{ fontSize: 16, marginRight: 8 }}>2.</Text>
+            <Text style={{ fontSize: 16 }}>
+              See submitted requests for loans from users
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 8,
+            }}
+          >
+            <Text style={{ fontSize: 16, marginRight: 8 }}>3.</Text>
+            <Text style={{ fontSize: 16 }}>
+              Accept and reject requests for loans you created
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 8,
+            }}
+          >
+            <Text style={{ fontSize: 16, marginRight: 8 }}>4.</Text>
+            <Text style={{ fontSize: 16 }}>
+              Contact users who submitted for your loans via phone or email
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 8,
+            }}
+          >
+            <Text style={{ fontSize: 16, marginRight: 8 }}>5.</Text>
+            <Text style={{ fontSize: 16 }}>See statistics</Text>
+          </View>
+        </View>
+      </View>
     </View>
-    <View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 8}}>
-      <Text style={{fontSize: 16, marginRight: 8}}>2.</Text>
-      <Text style={{fontSize: 16}}>Submit a request for loans</Text>
-    </View>
-    <View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 8}}>
-      <Text style={{fontSize: 16, marginRight: 8}}>3.</Text>
-      <Text style={{fontSize: 16}}>View pending and accepted loans you submitted</Text>
-    </View>
-  </View>
-  <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 16}}>
-    <View style={{width: 24, height: 24, backgroundColor: '#05445E', borderRadius: 12, marginRight: 8}} />
-    <Text style={{fontSize: 18, fontWeight: 'bold'}}>As an admin you can:</Text>
-  </View>
-  <View>
-    <View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 8}}>
-      <Text style={{fontSize: 16, marginRight: 8}}>1.</Text>
-      <Text style={{fontSize: 16}}>Create a new loan in the system</Text>
-</View>
-<View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 8}}>
-<Text style={{fontSize: 16, marginRight: 8}}>2.</Text>
-<Text style={{fontSize: 16}}>See submitted requests for loans from users</Text>
-</View>
-<View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 8}}>
-<Text style={{fontSize: 16, marginRight: 8}}>3.</Text>
-<Text style={{fontSize: 16}}>Accept and reject requests for loans you created</Text>
-</View>
-<View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 8}}>
-<Text style={{fontSize: 16, marginRight: 8}}>4.</Text>
-<Text style={{fontSize: 16}}>Contact users who submitted for your loans via phone or email</Text>
-</View>
-<View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 8}}>
-<Text style={{fontSize: 16, marginRight: 8}}>5.</Text>
-<Text style={{fontSize: 16}}>See statistics</Text>
-</View>
-</View>
-</View>
- </View>
   );
 };
 
--- server/controllers/customer.controller.js
+++ server/controllers/customer.controller.js
@@ -9,7 +9,23 @@ const AdminRole = db.admin;
 const CostumerRole = db.costumer;
 const Request = db.request;
 const Loan = db.loan;
-
+/**
+ * @Controller GET Request paging API for searching loans.
+ * In this controller, we expect to get query params in order to allow
+ * paging, if we didnt get the query params that we put bellow, then we
+ * assume that the params are { page = 1, limit = 10 }.
+ * @param page a param send with the query to specify which page to return.
+ * @param limit a param send with the query to specify the limit in the page.
+ * we also expect for params in the body of the request, see bellow:
+ * @param name: The name of the new loan.
+ * @param min: The minimum amount of the loans.
+ * @param max: The maximum amount of the loans.
+ * @param interest: The interest of the loans.
+ * @param loanRepayment: The loan repayment time of the loans.
+ * @param info: The information of the loans,
+ * @return {loans, totalPages, currentPage}, loans is for the loans in the page given, totalPages is for the number of pages,
+ * and currentPage is for the page that has been requested.
+ */
 exports.costumerGetAllLoans = (req, res) => {
   const { page = 1, limit = 10 } = req.query;
   let data = {
@@ -48,7 +64,10 @@ exports.costumerGetAllLoans = (req, res) => {
       }
     });
 };
-
+/**
+ * @Controller POST request fot a new loan request.
+ * @return message if successful or error.
+ */
 exports.costumerRequest = (req, res) => {
   Loan.findById(req.body.loan, (err, loan) => {
     if (err) {
@@ -62,8 +81,6 @@ exports.costumerRequest = (req, res) => {
             if (err) {
               res.status(404).send({ error: "can't find admin" });
             } else {
-              // let dateObject = new Date();
-              // let date = dateObject.getDate()+"/"+dateObject.getMonth()+"/"+dateObject.getYear();
               let date = moment().format("MMM Do YY");
               const request = new Request({
                 details: loan.name + "\n Amount: " + loan.amount + "₪",
@@ -87,7 +104,16 @@ exports.costumerRequest = (req, res) => {
     }
   });
 };
-
+/**
+ * @Controller GET Request paging API for getting all the requests that an user has.
+ * In this controller, we expect to get query params in order to allow
+ * paging, if we didnt get the query params that we put bellow, then we
+ * assume that the params are { page = 1, limit = 10 }.
+ * @param page a param send with the query to specify which page to return.
+ * @param limit a param send with the query to specify the limit in the page.
+ * @return {requests, totalPages, currentPage}, requests is for the requests in the page given, totalPages is for the number of pages,
+ * and currentPage is for the page that has been requested.
+ */
 exports.costumerGetAllRequests = (req, res) => {
   const { page = 1, limit = 10 } = req.query;
   User.findById(req.userId, (err, user) => {
